850159461b2bd760cd2cb702ef3f3304f5317518
Turma2 <cfc@ufrj.br>
2012-05-11 16:14:58 -0300

    Mudança para quebrar o teste.

--- TesteVendas/TesteCalculaComissao.cs
+++ TesteVendas/TesteCalculaComissao.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,7 +37,7 @@ namespace TesteVendas
         [TestMethod]
         public void TestVenda500ReaisCalculaComissaoIguala25()
         {
-            double valorComissao = 25;
+            double valorComissao = 26;
             double valorVendas = 500;
             double valorCalculado;
 
